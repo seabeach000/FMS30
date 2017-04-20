4c8a3912ec73d772b1cbb518d920dd32f4faec7c
wangxingang <wangxingang@DESKTOP-F5CPG2I>
2017-04-20 14:16:06 +0800

    [html_producer] #525 Fixed bug where only URL:s with . in them where recognized.

--- main/modules/html/producer/html_producer.cpp
+++ main/modules/html/producer/html_producer.cpp
@@ -604,9 +604,9 @@ spl::shared_ptr<core::frame_producer> create_producer(
 		const core::frame_producer_dependencies& dependencies,
 		const std::vector<std::wstring>& params)
 {
-	const auto filename			= env::template_folder() + params.at(0) + L".html";
-	const auto found_filename	= find_case_insensitive(filename);
-	const auto html_prefix		= boost::iequals(params.at(0), L"[HTML]");
+	const auto filename = env::template_folder() + params.at(0) + L".html";
+	const auto found_filename = find_case_insensitive(filename);
+	const auto html_prefix = boost::iequals(params.at(0), L"[HTML]");
 
 	if (!found_filename && !html_prefix)
 		return core::frame_producer::empty();
@@ -615,7 +615,7 @@ spl::shared_ptr<core::frame_producer> create_producer(
 		? L"file://" + *found_filename
 		: params.at(1);
 
-	if (!html_prefix && (!boost::algorithm::contains(url, ".") || boost::algorithm::ends_with(url, "_A") || boost::algorithm::ends_with(url, "_ALPHA")))
+	if (!html_prefix || boost::algorithm::ends_with(url, "_A") || boost::algorithm::ends_with(url, "_ALPHA"))
 		return core::frame_producer::empty();
 
 	return core::create_destroy_proxy(spl::make_shared<html_producer>(
